7d987ac60eebcedcb13f7b436af548ba43f6aa30
faisal arkan <faisalarkan21@gmail.com>
2019-10-18 10:07:41 +0700

    add new changes on flexbox

--- App.js
+++ App.js
@@ -1,102 +1,38 @@
 import React from 'react';
-import {Text, View, Button} from 'react-native';
-import {Provider} from 'react-redux';
 import {createAppContainer} from 'react-navigation';
 import {createStackNavigator} from 'react-navigation-stack';
 import {createBottomTabNavigator} from 'react-navigation-tabs';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import FlatListBasics from './src/pages/list-users/IndexListUsers';
-import configureStore from './src/store/configStore';
-import LoginScreen from './src/pages/login/IndexLogin';
+import {Provider} from 'react-redux';
 import HomeScreen from './src/pages/home/IndexHome';
+import FlexDirectionBasics from './src/pages/learn-flexbox/IndexFlexbox';
+import FlatListBasics from './src/pages/list-users/IndexListUsers';
+import LoginScreen from './src/pages/login/IndexLogin';
+import configureStore from './src/store/configStore';
 
-class SettingsScreen extends React.Component {
-  render() {
-    return (
-      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-        <Text>Settings!</Text>
-      </View>
-    );
-  }
-}
-
-const TabNavigator = createBottomTabNavigator(
-  {
-    Home: HomeScreen,
-    Settings: SettingsScreen,
-    ListUsers: FlatListBasics,
-  },
-  {
-    initialRouteName: 'Login',
-  },
-);
+const TabNavigator = createBottomTabNavigator({
+  Home: HomeScreen,
+  ListUsers: FlatListBasics,
+});
 
 const AppNavigator = createStackNavigator(
   {
-    Splash: {
-      screen: SplashScreen,
-      navigationOptions: {
-        header: null,
-      },
-    },
-    Welcome: {
-      screen: WelcomeScreen,
-      navigationOptions: {
-        header: null,
-      },
-    },
     Login: {
       screen: LoginScreen,
       navigationOptions: {
         title: 'Login',
-        headerStyle: {
-          backgroundColor: Color.grey01,
-          elevation: 0,
-          marginTop: 24,
-        },
         headerTintColor: '#fff',
-        headerTitleStyle: {
-          fontWeight: 'bold',
-          width: '72%',
-          textAlign: 'center',
-        },
       },
     },
-    Register: {
-      screen: RegisterScreen,
-      navigationOptions: {
-        title: 'Register',
-        headerStyle: {
-          backgroundColor: Color.grey01,
-          elevation: 0,
-          marginTop: 24,
-        },
-        headerTintColor: '#fff',
-        headerTitleStyle: {
-          fontWeight: 'bold',
-          width: '72%',
-          textAlign: 'center',
-        },
-      },
-    },
-    App: {
-      screen: bottomNavigator,
-      navigationOptions: {
-        header: null,
-      },
-    },
-    NowPlaying: {
-      screen: NowPlayingScreen,
-      navigationOptions: {
-        header: null,
-      },
-    },
+    Home: TabNavigator,
+    ListUsers: FlatListBasics,
+    FlexBoxLearn: FlexDirectionBasics,
   },
   {
-    initialRouteName: 'Splash',
+    initialRouteName: 'FlexBoxLearn',
   },
 );
-let Navigation = createAppContainer(TabNavigator);
+
+let Navigation = createAppContainer(AppNavigator);
 
 const store = configureStore();
 
--- src/pages/login/IndexLogin.js
+++ src/pages/login/IndexLogin.js
@@ -3,9 +3,10 @@ import {Text, View} from 'react-native';
 
 class LoginScreen extends React.Component {
   render() {
+    console.log('this.props', this.props);
     return (
       <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-        <Text>Login!</Text>
+        <Text onPress={() => this.props.navigation.navigate('Home')}>Login!</Text>
       </View>
     );
   }
